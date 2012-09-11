35b14fddd11ab7d3b28974b6f17e0bd11a309ec7
Ruby <Ruby@raspberry.me.uk>
2012-09-11 23:47:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ebe57e6b7938bd47c599e3059207c4fb720dd61c8bf3ee828886258e3a624506da8e5e7f7dc3375605b084ff65f4e53e64a571f235dc6ffb1febee36ac2ec54b'
